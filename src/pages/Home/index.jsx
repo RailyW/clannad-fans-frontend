52730b10676e1920b01b5b4fee9ca3608b45c77e
Jeff Wu <vinorin977@gmail.com>
2025-10-08 00:52:01 +1100

    feat: add home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import './style.less';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to CLANNAD Fans</h1>
+      <p>This is the home page of CLANNAD fans community.</p>
+    </div>
+  );
+}
+
+export default Home;

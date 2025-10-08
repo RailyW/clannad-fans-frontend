24d0cc621bee60ecfe3687fd5ece56a4601189ea
Jeff Wu <vinorin977@gmail.com>
2025-10-09 01:10:54 +1100

    feat: reset

--- src/pages/Home/SectionCharacter.jsx
+++ src/pages/Home/SectionCharacter.jsx
@@ -3,22 +3,10 @@ import CharacterSelector from '../../components/CharacterSelector';
 import { useState } from 'react';
 
 const SectionCharacter = () => {
-  const [selectedCharacter, setSelectedCharacter] = useState(null);
-
-  const handleCharacterSelect = (character) => {
-    setSelectedCharacter(character);
-    console.log('选中角色:', character);
-  };
-
   return (
     <div className="section-character">
       {/* 左侧信息区域 */}
-      <div className="info-area">
-        <CharacterSelector onSelect={handleCharacterSelect} />
-        <div className="character-details">
-          {/* 角色详细信息将在这里显示 */}
-        </div>
-      </div>
+      <div className="info-area"></div>
 
       {/* 右侧立绘区域 */}
       <div className="illustration-area"></div>
